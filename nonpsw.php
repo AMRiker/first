2c2620b092aed91860691acfe8c3f0e9b03ce2e8
Riker <maslov-ai-work@mail.ru>
2022-08-11 20:37:23 +0300

    мой первый коммит, в нем нет изменений

---
+++ nonpsw.php
@@ -0,0 +1,4 @@
+<?php
+echo 'your password is not confirmed';
+echo "<a href='index.html'> try again </a>";
+?>
---
+++ short.php
@@ -0,0 +1,5 @@
+<?php
+echo 'your password is too short';
+echo "<a href='index.html'> try again </a>";
+
+?>
