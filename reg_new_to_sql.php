ebe2d9066399b0e7b8aa65a78ab8004c393a2f04
Riker <maslov-ai-work@mail.ru>
2022-08-31 20:12:38 +0300

    форматированный по красоте

--- reg_new_to_sql.php
+++ reg_new_to_sql.php
@@ -1,26 +1,27 @@
 <?php
 require __DIR__ . '/User.php';
 //создаем ноывый объект типа user
-$New_user = new User ($_POST["login"],$_POST["password"],$_POST["confirm_password"],
+$user = new User ($_POST["login"],$_POST["password"],$_POST["confirm_password"],
 	$_POST["username"],$_POST["age"],$_POST["sex"]);
 
 // если я правильно понял что нужные переменные получили
 // из формы регистрации свои значения в квадратных скобках и сразу попали в класс
 echo '<pre>';
-var_dump($New_user);
+var_dump($user);
 echo '</pre>';
 
 
 //создаем функцию подключения и записи в БД
-function connect_to_bd(){
-	$db_host = "localhost";
-	$db_user = "root"; // Логин БД
-	$db_password = "root"; // Пароль БД
-	$db_base = 'users1'; // Имя БД
-	$db_table = "user"; // Имя Таблицы БД
+function connectToBd()
+{
+	$dbHost = "localhost";
+	$dbUser = "root"; // Логин БД
+	$dbPassword = "root"; // Пароль БД
+	$dbBase = 'users1'; // Имя БД
+	$dbTable = "user"; // Имя Таблицы БД
 	try {
 		// Подключение к базе данных, используем адрес БД (локальный в данном случае) логин и пароль от mysql,  того что в моем openserver
-		$db = new PDO("mysql:host=$db_host;dbname=$db_base", $db_user, $db_password);
+		$db = new PDO("mysql:host=$dbHost;dbname=$dbBase", $dbUser, $dbPassword);
 
 
 		// Устанавливаем корректную кодировку (не знаю зачем скопировал из учебника как есть)
@@ -45,32 +46,37 @@ function connect_to_bd(){
 }
 //создаем функцию для внесения данных учетной записи в таблицу
 
-function wr_to_users1($New_user, $db)
+function wrToUsers1($user, $db)
 {
 
-// Параметры для подключения
-// инициализируем ряд переменных для подключения к базе sql
-	$users_data = array ('login' => $New_user->login, 'password' => $New_user->password,
-		'name' => $New_user->username, 'age' => $New_user->age, 'sex' => $New_user->sex);
+	// Параметры для подключения
+	// инициализируем ряд переменных для подключения к базе sql
+	$usersData = [
+		'login' => $user->login,
+		'password' => $user->password,
+		'name' => $user->username,
+		'age' => $user->age,
+		'sex' => $user->sex
+	];
 
 	echo '<pre>';
-var_dump($users_data);
+	var_dump($usersData);
 	echo '</pre>';
 
-//если я правильно понял то теперь у нас получился массив из данных пользователя,
-//где ключи это имя полей, а значения взяты из переменных формы
-//теперь формируем запрос в базу
+	//если я правильно понял то теперь у нас получился массив из данных пользователя,
+	//где ключи это имя полей, а значения взяты из переменных формы
+	//теперь формируем запрос в базу
 
 	$query = $db->prepare ("INSERT INTO user (login, password, name, age, sex) values (:login, :password, :name, :age, :sex)");
 
-// Выполняем запрос с данными
-	$query->execute($users_data);
+	// Выполняем запрос с данными
+	$query->execute($usersData);
 
 }
 
-if ($New_user->valid_data()){
-	$db = connect_to_bd();
-	wr_to_users1($New_user, $db);
+if ($user->valid_data()){
+	$db = connectToBd();
+	wrToUsers1($user, $db);
 }
 else{
 	header('Location reg_error.php' );
