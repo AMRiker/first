516ec7e9b6cd6af1cb46198e1a0d61256d75567d
Riker <maslov-ai-work@mail.ru>
2022-09-07 20:34:58 +0300

    обозначили класс в check user

--- check_user_sql.php
+++ check_user_sql.php
@@ -1,18 +1,20 @@
 <?php
 require_once  __DIR__ . '/Connect.php';
-$login = $_POST["login"];
-$password=$_POST["password"];
+require_once  __DIR__ . '/User.php';
+$user = new User();
+$user->login = $_POST["login"];
+$user->password=$_POST["password"];
 $connect = new Connect();
 $db = $connect->connectToBd();
 $checkUser = $db -> prepare("SELECT * FROM user WHERE login=:login");
-$checkUser->bindParam("login", $login, PDO::PARAM_STR);
+$checkUser->bindParam("login", $user->login, PDO::PARAM_STR);
 $checkUser->execute();
 $result = $checkUser->fetch(PDO::FETCH_ASSOC);
 
 echo '<pre>';
 var_dump($result);
 echo '</pre>';
-if ($password == $result['password']){
+if ($user->password == $result['password']){
 	print 'Поздравляю вы прошли авторизацию';
 }
 ?>
