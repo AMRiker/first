c25c88703db3af19962f8ce32cecd723d50860f9
Riker <maslov-ai-work@mail.ru>
2022-09-14 20:47:55 +0300

    сделали первую форму в контртоллере

---
+++ controller/UserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace controller;
+
+class UserController
+{
+    public function wrToUsers1($user, $db) // todo: убрать ее в класс user
+    {
+
+        // Параметры для подключения
+        // инициализируем ряд переменных для подключения к базе sql
+        $usersData = [
+            'login' => $user->login,
+            'password' => $user->password,
+            'name' => $user->username,
+            'age' => $user->age,
+            'sex' => $user->sex
+        ];
+
+        echo '<pre>';
+        var_dump($usersData);
+        echo '</pre>';
+
+        //если я правильно понял то теперь у нас получился массив из данных пользователя,
+        //где ключи это имя полей, а значения взяты из переменных формы
+        //теперь формируем запрос в базу
+
+        $query = $db->prepare ("INSERT INTO user (login, password, name, age, sex) values (:login, :password, :name, :age, :sex)");
+
+        // Выполняем запрос с данными
+        $query->execute($usersData);
+
+    }
+
+    public function actionRegistration()
+    {
+        require_once  __DIR__ . '/../model/User.php';
+        require_once  __DIR__ . '/../model/Connect.php';
+//создаем ноывый объект типа user
+        $user = new \User();
+        $user-> fill(
+            $_POST["login"],
+            $_POST["password"],
+            $_POST["confirm_password"],
+            $_POST["username"],
+            $_POST["age"],
+            $_POST["sex"]
+        );
+        // если я правильно понял что нужные переменные получили
+        // из формы регистрации свои значения в квадратных скобках и сразу попали в класс
+        //echo '<pre>';
+        //var_dump($user);
+        //echo '</pre>';
+
+
+
+        if ($user->validData()) {
+            $connect = new \Connect();
+            $db = $connect->connectToBd();
+            $this->wrToUsers1($user, $db);
+        }
+        else {
+            include __DIR__ . '/view/regError.php';
+        }
+    }
+}
--- index.php
+++ index.php
@@ -6,7 +6,9 @@ echo '<pre>';
 var_dump($_SERVER['REQUEST_URI']);
 echo '<pre>';
 if ($_SERVER ['REQUEST_URI'] == '/index.php/registration'){
-        require_once __DIR__ . '/reg_new_to_sql.php';
+        require_once __DIR__ . '/controller/userController.php';
+        $controller = new \controller\UserController();
+        $controller->actionRegistration();
 }
 if ($_SERVER ['REQUEST_URI'] == '/index.php/enter'){
     require_once __DIR__ . '/check_user_sql.php';
