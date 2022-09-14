92dfc5d1dea08f6f3657f7c2787d951f0e7259ad
Riker <maslov-ai-work@mail.ru>
2022-09-14 20:16:48 +0300

    начали роутинг

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-require_once  __DIR__ . '/view/startPage.html';
+if ($_SERVER ['REQUEST_URI'] == '/'){
+    require_once __DIR__ . '/view/startPage.html';
+}
+echo '<pre>';
+var_dump($_SERVER['REQUEST_URI']);
+echo '<pre>';
+if ($_SERVER ['REQUEST_URI'] == '/index.php/registration'){
+        require_once __DIR__ . '/reg_new_to_sql.php';
+}
